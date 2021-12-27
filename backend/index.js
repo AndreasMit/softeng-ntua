df06102c0cc8caea123ddf7fb23e19f841386ee7
Ubuntu <softeng@softeng.b1j4ylyo04qetkf1j4z05muv1a.ax.internal.cloudapp.net>
2021-12-27 11:15:50 +0000

    more dependencies installed

--- backend/index.js
+++ backend/index.js
@@ -4,4 +4,4 @@ const port = 9103
 
 app.listen(port,() => { console.log('app listening on port', port);} );
 
-app.get('/', (req,res) => { res.send("hello world")} );
+app.get('/', (req,res) => { res.send("Giati den vlepw progress sto YouTrack??")} );
