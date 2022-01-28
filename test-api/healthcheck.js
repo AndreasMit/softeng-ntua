3321065ffd541e1b2d25ab36ef7a60209bbf4bb3
Marinos736 <el18135@mail.ntua.gr>
2022-01-29 00:17:05 +0200

    test for healthcheck

---
+++ test-api/healthcheck.js
@@ -0,0 +1,13 @@
+var assert = require('assert'),
+https = require('https');
+
+describe('healthcheck', function () {
+it('check status 200', function (done) {
+    process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
+  https.get('https://localhost:9103/interoperability/api/admin/healthcheck', function (res) {
+    assert.equal(200, res.statusCode);
+    done();
+  });
+});
+});
