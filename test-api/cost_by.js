d749690f8df5223d0be43e2f0cbc455591be8d83
Marinos736 <el18135@mail.ntua.gr>
2022-01-29 19:48:23 +0200

    test for cost_by (PassesCost)

--- test-api/cost_by.js
+++ test-api/cost_by.js
@@ -1,3 +1,4 @@
+
 var assert = require('assert');
 const https = require('https');
 let chai = require('chai');
@@ -6,19 +7,17 @@ chai.use(require('chai-things'));
 
 
 
-describe('/GET ChargesBy', function () {
+describe('/GET PassesCost', function () {
     process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
-
-    it('GET all the charges between 1/1/2020 and 21/12/2020', function(done) {
+    it('GET all the info between 1/11/2020 and 30/11/2021', function(done) {
       
-        https.get('https://localhost:9103/interoperability/api/ChargesBy/AO/20200101/20201221',function(res){
+        https.get('https://localhost:9103/interoperability/api/PassesCost/AO/EG/20201101/20211130',function(res){
             assert.equal(200, res.statusCode);
             res.on('data', (d) => {
                // process.stdout.write(d);
                
                var datastring = d.toString('utf8');
                var myObj = JSON.parse(datastring);
-                myObj.should.contain.a.thing.with.property('VisitingOperator');
                 myObj.should.contain.a.thing.with.property('NumberOfPasses');
                 myObj.should.contain.a.thing.with.property('PassesCost');
               });
